987212e73ad0652d1534a92634798c68ed0f477e
Benni <berrouane@techfak.uni-bielefeld.de>
2014-12-14 14:55:42 +0100

    Bluetooth is now checked onResume

--- app/src/main/java/errouane/benjamin/pushanalizer/activities/MainActivity.java
+++ app/src/main/java/errouane/benjamin/pushanalizer/activities/MainActivity.java
@@ -45,6 +45,13 @@ public class MainActivity extends Activity {
     protected void onResume() {
         super.onResume();
 
+        // Ensures Bluetooth is available on the device and it is enabled. If not,
+        // displays a dialog requesting user permission to enable Bluetooth.
+        if (bluetoothAdapter == null || !bluetoothAdapter.isEnabled()) {
+            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+        }
+
         adapter.clear();
     }
 
@@ -100,13 +107,6 @@ public class MainActivity extends Activity {
         // Initializes Bluetooth adapter.
         final BluetoothManager bluetoothManager = (BluetoothManager) getSystemService(Context.BLUETOOTH_SERVICE);
         bluetoothAdapter = bluetoothManager.getAdapter();
-
-        // Ensures Bluetooth is available on the device and it is enabled. If not,
-        // displays a dialog requesting user permission to enable Bluetooth.
-        if (bluetoothAdapter == null || !bluetoothAdapter.isEnabled()) {
-            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-            startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
-        }
     }
 
     private void scan() {
